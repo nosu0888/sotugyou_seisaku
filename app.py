645077675a32e0a4badb5d0e06e93cde7dcc454a
Airi Yamashiro <99.ili.jcw@gmail.com>
2021-02-28 16:38:42 +0900

    パイソン動かん

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
 from flask import Flask, render_template, request, redirect, session
 import sqlite3
 app = Flask(__name__)
-app.secret_key = "sunabaco"
+app.secret_key = "airi"
+
+
+@app.route('/regist')
+def regist_get():
+    return render_template("regist.html")
+
+@app.route('/regist' , methods=["POST"])
+def regist_post():
+    input_name = request.form.get("member_name")
+    input_password = request.form.get("member_password")
+
+    # flasktest.dbに接続
+    conn = sqlite3.connect("flasktest.db")
+    # DBの中を操作できるようにする
+    c = conn.cursor()
+    # SQLを実行（DBから値を取得）
+    c.execute("insert into member values(null , ? , ?)",(input_name, input_password))
+    # DBの変更内容保存する
+    conn.commit()
+    # DBとの接続を終える
+    c.close()
+
+    return "アカウント登録完了！"
